20d3e9cb53add00bd7523b4134452c61e15e9a3d
Andy <andy@polasek.com>
2017-10-12 14:06:40 -0500

    Add project files.

---
+++ Scheduler/Job.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Scheduler
+{
+    class Job
+    {
+        private static int count;
+        private String name;
+        private int time;
+        private int day;
+        private int length;
+        private List<Person> people;
+        private int ID;
+        private Person doer;
+
+        public Job(String name, int day, int time, int length)
+        {
+            this.name = name;
+            this.time = time;
+            this.day = day;
+            this.length = length;
+            this.ID = count++;
+            people = new List<Person>(0);
+        }
+
+        public void addPerson(Person p)
+        {
+            this.people.Add(p);
+        }
+
+        public bool conflicts(Job j)
+        {
+            if(conDay(j.day) && conTime(j.time,j.length))
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public bool conTime(int time, int length)
+        {
+            return  this.time == time
+                    || (this.time - time < 0 && Math.Abs(this.time - time) <= this.length)
+                    || (this.time - time > 0 && Math.Abs(this.time - time) <= length);
+        }
+
+        public bool conDay(int day) { return this.day == day; }
+    }
+}
---
+++ Scheduler/Person.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Scheduler
+{
+    class Person
+    {
+        private String name;
+        private int avail;
+        private int hours;
+        private Day[] week;
+        private List<Job> jobsList;
+
+        public Person(String name)
+        {
+            this.name = name;
+            this.avail = 0;
+            this.hours = 0;
+            jobsList = new List<Job>(0);
+            for (int i = 0; i < 7; i++)
+            {
+                week[i] = new Day();
+            }
+        }
+
+        //returns true if successful add, false if not
+        public bool addJob(Job j)
+        {
+            //check for conflicts
+            foreach(Job job in jobsList)
+            {
+                if(j.conflicts(job))
+                {
+                    return false;
+                }
+            }
+            jobsList.Add(j);
+            j.addPerson(this);
+            return true;
+        }
+    }
+}
